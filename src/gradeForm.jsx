0adb744a29ea0735be0528acb8db91df025e72cb
samuel mutinda <mutindasamuel01@gmail.com>
2024-02-13 17:03:04 +0300

    paystack

--- src/gradeForm.jsx
+++ src/gradeForm.jsx
@@ -3,6 +3,7 @@ import { SubjectAndGrade } from "./subjectAndGrade";
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Popup } from "./popup";
+import axios from "axios";
 
 GradeForm.propTypes = {
     onSubmit: PropTypes.func.isRequired,
@@ -29,12 +30,19 @@ const subjectValues = [
     "uw"
 ];
 
+/* tiny pesa api requirements*/
+// const API_KEY = 'QBPIA8z6whK';
+const API_URL = 'https://api.paystack.co/charge';
+// const ACC_NUMBER = '200';
+const AMOUNT = '50';
 
 
 export function GradeForm({ onSubmit }) {
     const [selectedSubjectsData, setSelectedSubjectsData] = useState([]);
     const [mpesaNumber, setMpesaNumber] = useState("");
     const [showPopup, setShowPopup] = useState(false);
+    const [paymentConfirmed, setPaymentConfirmed] = useState(false);
+    const [reference, setReference] = useState("")
     
     const handleSubjectStateChange = (index, selectedSubject, selectedGrade) => {
         const updatedSelectedSubjectsData = [...selectedSubjectsData];
@@ -48,21 +56,75 @@ export function GradeForm({ onSubmit }) {
 
     const results = [];
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         setShowPopup(true);
+        /* send stk push request*/
+        await sendStkRequest();
     };
       
     // THIS FUNCTION IS CALLED WHEN THE BUTTON TO CONFIRM PAYMENT IS CALLED
-    const handleConfirmPayment = () => {
+    const handleConfirmPayment = async () => {
+        try {
+            const {data} = await axios.get(`https://api.paystack.co/transaction/verify/:${reference}`, {
+                headers: {
+                    Authorization: "Bearer sk_live_8ed931223ca2e35ca44051565a8b4a6c588ab694",
+                    'Content-Type': 'application/json',
+                }
+            })
+            if(data){
+                // when the payment has been confirmed it then calculates the results
+                console.log(data)
+            }else{
+                // This means that the payment has not been made
+                console.log("There is no data");
+            }
+            
+        } catch (error) {
+            console.log(error.response);
+        }        
+    };
+
+    const submitResults = () => {
         setShowPopup(false); 
         // THEN IT REDIRECTS TO THE RESULTS PAGE. MAKE CHANGES HERE
         // THE onSubmit FUNCTION SHOULD ONLY BE CALLED IF THE PAYMENT HAS BEEN MADE
         onSubmit(results);
-    };
+    }
 
     const handleClosePopup = () => {
         setShowPopup(false); 
+    }
+
+    async function sendStkRequest() {
+        try {
+            const {data} = await axios.post(API_URL, {
+                amount: "200",
+                email: "mutindasamuel01@gmail.com",
+                currency: "KES",
+                mobile_money: { 
+                  phone: "+254713241864", 
+                  provider: "mpesa"
+                } 
+              
+            }, {
+                headers: {
+                    Authorization: "Bearer sk_live_8ed931223ca2e35ca44051565a8b4a6c588ab694",
+                    'Content-Type': 'application/json',
+                }
+            })
+            if(data){
+                // when the payment has been confirmed it then calculates the results
+                console.log(data.data.reference)
+                setReference(data.data.reference)
+            }else{
+                // This means that the payment has not been made
+                console.log("There is no data")
+            }
+            
+        } catch (error) {
+            console.log(error.response);
+        }        
     }
 
     return (
